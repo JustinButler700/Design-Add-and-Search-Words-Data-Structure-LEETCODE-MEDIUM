a7ecd61069ff308568ddd424826d81bebc9d857a
Justin Butler <57680670+JustinButler700@users.noreply.github.com>
2021-11-12 13:27:02 -0800

    Create Solution1.java

---
+++ Solution1.java
@@ -0,0 +1,46 @@
+//HashSet solution
+class WordDictionary {
+    HashSet<String> dict;
+    boolean singleChar = false;
+    public WordDictionary() {
+        dict = new HashSet<String>();
+    }
+    public void addWord(String word) {
+        if(word.length()==1){singleChar = true;} // improve search functionality for single chars
+        dict.add(word);
+    }
+    public boolean search(String word) {
+        if(word.contains("."))
+        {
+            if(word.equals(".")) // edge case that slows us down
+            {return singleChar;}
+            char[] arr = word.toCharArray();
+            for(String i : dict) // search for match
+            {
+                if(i.length() == arr.length)
+                {
+                    StringBuilder curr = new StringBuilder();
+                    for(int j = 0; j < arr.length; j++)
+                    {
+                        if(arr[j]=='.')
+                        {
+                            curr.append('.');
+                        }
+                        else
+                        {
+                            curr.append(i.charAt(j));
+                        }
+                    }
+                    if(curr.toString().equals(word))
+                    {
+                        return true;
+                    }
+                }
+                
+            }
+        }
+        return dict.contains(word);
+        
+    }
+        
+}
